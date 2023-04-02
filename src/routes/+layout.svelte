8c923f4ce78cfc853b43bc100295630f5ffd9d87
Wong Wen Yean <wongwyean3@gmail.com>
2023-04-02 15:35:17 +0800

    Add navbar and footer components in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '../app.css';
+	import Footer from '../libs/components/Footer.svelte';
+	import Navbar from '../libs/components/Navbar.svelte';
 </script>
 
+<!-- Navbar -->
+<!-- Change color here -->
+<Navbar />
+
 <slot />
+
+<Footer />
+<!-- Change footer -->

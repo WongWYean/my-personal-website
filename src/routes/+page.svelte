c5d774e1d7b98ee4eadedc0cf43f7982e28bc8d1
Wong Wen Yean <wongwyean3@gmail.com>
2023-04-02 18:03:53 +0800

    Add color picker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
-	// TODO LIST
-	// I need to setup my color
-	// Add Color picker func here
+	import { onMount } from 'svelte';
+
+	let color: string = '#000000';
+
+	function handleColorChange(event: Event) {
+		color = (event.target as HTMLInputElement).value;
+	}
+
+	onMount(() => {
+		console.log(`Initial color: ${color}`);
+	});
 </script>
 
-<!-- Colour Picker -->
+<label for="color-picker">Select a color:</label>
+<input type="color" id="color-picker" bind:value={color} on:change={handleColorChange} />
+
+<p>You chose: {color}</p>
+
+<!-- Color Picker together with conversion -->
+<!-- Color Picker with Name -->
+<!-- Add variations -->
+<!-- Add color harmonies -->
+<!-- Add color blindness -->
+<!-- Add contrast checker -->

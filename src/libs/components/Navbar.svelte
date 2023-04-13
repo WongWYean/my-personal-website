24a4905f17c2735fdef8fe2d04baac86d9e1f626
Wong Wen Yean <wongwyean3@gmail.com>
2023-04-13 22:21:05 +0800

    add lang ts to script

--- src/libs/components/Navbar.svelte
+++ src/libs/components/Navbar.svelte
@@ -1,6 +1,10 @@
-<div class="navbar bg-base-100">
+<script lang="ts">
+</script>
+
+<div class="navbar">
 	<div class="navbar-start" />
 	<div class="navbar-center">
+		<!-- Add logic to redirect another page -->
 		<button class="btn btn-ghost normal-case text-xl">AzureEon</button>
 	</div>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import Footer from '../libs/components/Footer.svelte';
 	import Navbar from '../libs/components/Navbar.svelte';
